6487ae1a47fa2fb2e9cb4a1ab68366317b55a72c
Chris Mills <Chris@Chriss-MacBook-Air.local>
2015-12-08 23:03:14 -0500

    board evaluation function in primer1

--- synapsemonpy_primer1.py
+++ synapsemonpy_primer1.py
@@ -1,5 +1,5 @@
 #Pretraining the weights
-
+from __future__ import division
 from pybrain.tools.customxml.networkreader import NetworkReader
 from pybrain.tools.customxml.networkwriter import NetworkWriter
 from pybrain.datasets import SupervisedDataSet                                  
@@ -14,15 +14,34 @@ net = buildNetwork(97, 80, 1)
 
 def move_function(board):
     global net  
-
     #active player
-    if board.active == BLACK:
-        ds.addSample(board_to_input(board), max_value)
-        best_move = best_max_move
-    elif board.active == WHITE: 
-        ds.addSample(board_to_input(board), min_value)
-        best_move = best_min_move
+    #if board.active == BLACK:
+    #   ds.addSample(board_to_input(board), max_value)
+    #    best_move = best_max_move
+    #elif board.active == WHITE: 
+    #    ds.addSample(board_to_input(board), min_value)
+    #   best_move = best_min_move
+    boardString=board_to_input(board)
+    black_material=0
+    white_material=0
+    for i in range(32):
+        isKing=boardString[i+64]=='1'
+        if boardString[i]=='1':
+            if isKing:
+                black_material=black_material+2
+            else: 
+                black_material=black_material+1
+        if boardString[i+32]=='1':
+            if isKing:
+                white_material=white_material+2
+            else:
+                white_material=white_material+1
+    
+    board_val = black_material/(black_material+white_material)
 
+    #create a new dataset. Add a sample with board as input, value as output
+    ds = SupervisedDataSet(97, 1)
+    ds.addSample(boardString, board_val)
     trainer = BackpropTrainer(net, ds)
     trainer.train()
     NetworkWriter.writeToFile(net, 'SynapsemonPie/synapsemon_primer1.xml')
@@ -36,7 +55,7 @@ def board_to_input(board):
     WHITE_KING = 3
 
     if board.active == BLACK:
-        black_kings = board.backward[board.active]
+        black_kings = board.backwa rd[board.active]
         black_men = board.forward[board.active] ^ black_kings
         white_kings = board.forward[board.passive]
         white_men = board.backward[board.passive] ^ white_kings
